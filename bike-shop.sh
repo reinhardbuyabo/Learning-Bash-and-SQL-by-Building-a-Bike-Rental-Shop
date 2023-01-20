941baed5ea2a15b7b0fc71c302534660e6b9c637
reinhardbuyabo <buyabor@gmail.com>
2023-01-20 15:57:45 +0000

    Save progress: 1320.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -74,6 +74,7 @@ RENT_MENU() {
           INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers (name, phone) VALUES ('$CUSTOMER_NAME', '$PHONE_NUMBER')")
         fi
         # get customer_id
+        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE_NUMBER'")
 
         # insert bike rental
 
