5ef403b605aff373bf6691bbbf9f3b38852c44bf
reinhardbuyabo <buyabor@gmail.com>
2023-01-20 08:07:20 +0000

    Save progress: 990.1

---
+++ .freeCodeCamp/reset_files/bike-shop.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
+
+echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
+
+MAIN_MENU() {
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+
+  echo "How may I help you?" 
+  echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
+  read MAIN_MENU_SELECTION
+
+  case $MAIN_MENU_SELECTION in
+    1) RENT_MENU ;;
+    2) RETURN_MENU ;;
+    3) EXIT ;;
+    *) MAIN_MENU "Please enter a valid option." ;;
+  esac
+}
+
+RENT_MENU() {
+  # get available bikes
+  AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available = true ORDER BY bike_id")
+  echo "$AVAILABLE_BIKES"
+
+  # if no bikes available
+
+  # send to main menu
+
+}
+
+RETURN_MENU() {
+  echo "Return Menu"
+}
+
+EXIT() {
+  echo -e "\nThank you for stopping in.\n"
+}
+
+MAIN_MENU
